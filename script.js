337274d10c8fabd92ac5f19e215aa7c6998ef1ce
ZhdankovAlex <47736968+ZhdankovAlex@users.noreply.github.com>
2019-07-05 20:29:30 +0300

    smart_bot-strategy was added (but not complete)

--- script.js
+++ script.js
@@ -11,39 +11,129 @@ function randomBot()
 function masterBot()
 {
 	var id;
-	if(table[0])
+	if(table[0]=='human')
+	{
+		//если это первый ход бота
+		if(!table[1] && !table[2] && !table[3] && !table[4] && !table[5] && !table[6] && !table[7] && !table[8])
+		{
+			move(4, 'bot'); 
+			return;
+			/*
+				0 * *
+				* X *
+				* * *
+			*/
+		}
+		
+		//если это второй ход бота
+		if(table[1]=='human'&& !table[2] && !table[3] && !table[5] && !table[6] && !table[7] && !table[8])
+		{
+			move(2, 'bot');
+			return;
+			/*
+				0 0 X
+				* X *
+				* * *
+			*/
+		}
+		
+		if(table[2]=='human'&& !table[1] && !table[3] && !table[5] && !table[6] && !table[7] && !table[8])
+		{
+			move(1, 'bot');
+			return;
+			/*
+				0 X 0
+				* X *
+				* * *
+			*/
+		}
+		
+		if(table[3]=='human'&& !table[1] && !table[2] && !table[5] && !table[6] && !table[7] && !table[8])
+		{
+			move(6, 'bot');
+			return;
+			/*
+				0 * *
+				0 X *
+				X * *
+			*/
+		}
+		
+		if(table[5]=='human'&& !table[1] && !table[2] && !table[3] && !table[6] && !table[7] && !table[8])
+		{
+			move(2, 'bot');
+			return;
+			/*
+				0 * X
+				* X 0
+				* * *
+			*/
+		}
+		
+		if(table[6]=='human'&& !table[1] && !table[2] && !table[3] && !table[5] && !table[7] && !table[8])
+		{
+			move(2, 'bot');
+			return;
+			/*
+				0 * *
+				X X *
+				0 * *
+			*/
+		}
+		
+		if(table[7]=='human'&& !table[1] && !table[2] && !table[3] && !table[5] && !table[6] && !table[8])
+		{
+			move(6, 'bot');
+			return;
+			/*
+				0 * *
+				* X *
+				X 0 *
+			*/
+		}
+		
+		if(table[8]=='human'&& !table[1] && !table[2] && !table[3] && !table[5] && !table[6] && !table[7])
+		{
+			move(2, 'bot');
+			return;
+			/*
+				0 * *
+				* X *
+				* * 0
+			*/
+		}
+		//если это третий ход бота
+		//если это четвёртый ход бота		
+	}
+	if(table[1]=='human')
 	{
 		
 	}
-	if(table[1])
+	if(table[2]=='human')
 	{
 		
 	}
-	if(table[2])
+	if(table[3]=='human')
 	{
 		
 	}
-	if(table[3])
+	if(table[4]=='human')
 	{
 		
 	}
-	if(table[4])
+	if(table[5]=='human')
 	{
 		
 	}
-	if(table[5])
+	if(table[6]=='human')
 	{
 		
 	}
-	if(table[6])
+	if(table[7]=='human')
 	{
 		
 	}
-	if(table[7])
-	{
-		
-	}
-	if(table[8])
+	if(table[8]=='human')
 	{
 		
 	}
